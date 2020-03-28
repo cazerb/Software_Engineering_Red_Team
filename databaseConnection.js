e65318d381b00b956214ce0301fceb220075bd0f
akshaypatel <akshaypatel7@hotmail.com>
2020-03-28 15:23:23 -0400

    Added changes so that node will run the html files

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 let mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "database name",
+  host: "database1.c8lplhzedax4.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "admin123",
+  port: "3306",
   multipleStatements: true
 });
 
