66a952187b5750ccb12329bfa695218484179491
akshaypatel <akshaypatel7@hotmail.com>
2020-03-28 15:32:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 const path = require('path');
 
 app.use(express.static(path.join(__dirname,'count-ui')));
+
 app.use('/html',(req,res,next)=>{
 res.sendFile(path.join(__dirname,'count-ui','index.html'));
 res.sendFile(path.join(__dirname,'count-ui','style.css'));
